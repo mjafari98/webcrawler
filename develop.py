c0496ac923f08456b58f582d23f1205f7b90bf51
mohammad <m.jafari9877@gmail.com>
2018-12-21 22:00:41 +0330

    add example webpages to project and perform analysis on them

--- develop.py
+++ develop.py
@@ -1,24 +1,30 @@
 from bs4 import BeautifulSoup as bs
 import json
 import requests
+import re
+
 
 def adding(URI, dictionary, depth):
-    if depth > 0:
-        # Send GET request to take the HTML DOC from the URI
-        root_URI = requests.get(URI)
+    try:
+        if depth > 0:
+            # Send GET request to take the HTML DOC from the URI
+            # root_URI = requests.get(URI)
 
-        # Render HTML_DOC from string to beautifulsoup
-        soup = bs(root_URI.text, 'html.parser')
+            root_URI = open(URI, "r")
 
-        # Query on beautifulsoup to pull out hyperlinks
-        for link in soup.find_all('a'):
-            if len(str(link.get('href')))>3:
-                if str(link.get('href'))[0:4] == 'http' \
-                and link.get('href') != None \
-                and str(link.get('href')) not in dictionary:
-                    dictionary[str(link.get('href'))] = dict()
-                    if depth > 1:
-                        adding(str(link.get('href')), dictionary[str(link.get('href'))], depth-1)
+            # Render HTML_DOC from string to beautifulsoup
+            soup = bs(root_URI.read(), 'html.parser')
+
+            # Query on beautifulsoup to pull out hyperlinks
+            # for link in soup.findAll('a', attrs={'href': re.compile("^http[s]*://")}):
+            for link in soup.findAll('a'):
+                #print(str(link.get('href')))
+                dictionary[str(link.get('href'))] = dict()
+                if depth > 1:
+                    adding(str(link.get('href')), dictionary[str(link.get('href'))], depth-1)
+
+    except Exception as e:
+        pass
 
 
 # The tree of links appends here
