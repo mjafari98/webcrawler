1d7b826c1690d19298e500cf258cb3050502f2ce
mohammad <m.jafari9877@gmail.com>
2018-12-22 16:20:25 +0330

    some correction on app.py - works fine and faster now

--- app.py
+++ app.py
@@ -3,24 +3,32 @@ import json
 import requests
 import re
 
+ignoring_hosts = ['youtube',
+                  'telegram',
+                  'facebook',
+                  'twitter',
+                  ]
 
 def adding(URI, dictionary, depth):
     try:
         if depth > 0:
-            # Send GET request to take the HTML DOC from the URI
-            root_URI = requests.get(URI)
+            if any(host in URI for host in ignoring_hosts):
+                pass
+            else:
+                # Send GET request to take the HTML DOC from the URI
+                root_URI = requests.get(URI)
 
-            # Render HTML_DOC from string to beautifulsoup
-            soup = bs(root_URI.read(), 'html.parser')
+                # Render HTML_DOC from string to beautifulsoup
+                soup = bs(root_URI.text, 'html.parser')
 
-            # Query on beautifulsoup to pull out hyperlinks
-            for link in soup.findAll('a', attrs={'href': re.compile("^http[s]*://")}):
-                dictionary[str(link.get('href'))] = dict()
-                if depth > 1:
-                    adding(str(link.get('href')), dictionary[str(link.get('href'))], depth-1)
+                # Query on beautifulsoup to pull out hyperlinks
+                for link in soup.find_all('a', attrs={'href': re.compile("^http[s]*://")}):
+                    dictionary[str(link.get('href'))] = dict()
+                    if depth > 1:
+                        adding(str(link.get('href')), dictionary[str(link.get('href'))], depth-1)
 
     except Exception as e:
-        pass
+        print(e)
 
 
 # The tree of links appends here
