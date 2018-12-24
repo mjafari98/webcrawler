5c822b7c1e5a58b8b9a4e73cadcef1f2e500f6ab
mohammad <m.jafari9877@gmail.com>
2018-12-24 17:12:47 +0330

    fix regex and some minor bugs. (php links like '{}' removed, slashes '/' fix, unicode links fix and ...)

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import json
 import requests
-import re
+import regex
 
 ignoring_hosts = ['youtube',
                   'telegram',
@@ -20,30 +20,48 @@ domain_list = ['.ir',
                '.org',
                '.net',]
 
+ignoring_notation = ['\{',
+                     '\}']
+
 def relative_to_absolute(BASE_URI, links):
     deleting_list = list()
     for i in range(0, len(links)):
+
+        for char_counter in range(5):
+            if links[i][0] == "/":
+                links[i] = links[i][1:]
+            else:
+                break;
+
+        # append to delete list if the link is None(null)
         if links[i] == None:
             deleting_list.append(links[i])
-            pass
 
+        # append to delete list if length of the link is 0
         elif len(links[i]) < 1:
             deleting_list.append(links[i])
-            pass
 
+        elif '{' in links[i] or '\\x' in links[i]:
+            deleting_list.append(links[i])
+
+        # don't change anything if it has http at the first of the link
         elif links[i][:4] == 'http':
             pass
 
+        # don't change anything if it is a valid domain
         elif any(domain in links[i] for domain in domain_list):
             pass
+        
+        # if it's a url without hostname add the hostname to it
+        # elif links[i][0] == '/':
+        #     a = links[i]
+        #     if BASE_URI[-1] != '/':
+        #         links[i] = BASE_URI + a
+        #     elif BASE_URI[-1] == '/':
+        #         links[i] = BASE_URI[:-1] + a
 
-        elif links[i][0] == '/':
-            a = links[i]
-            if BASE_URI[-1] != '/':
-                links[i] = BASE_URI + a
-            elif BASE_URI[-1] == '/':
-                links[i] = BASE_URI[:-1] + a
 
+        # if it's a url without hostname add the hostname to it
         else:
             a = links[i]
             if BASE_URI[-1] != '/':
@@ -60,26 +78,29 @@ def adding(URI, dictionary, depth):
     # try:
     if depth > 0:
         if any(host in URI for host in ignoring_hosts) or \
-           any(format in URI for format in ignoring_formats):
+           any(format in URI for format in ignoring_formats) or \
+           any(notation in URI for notation in ignoring_notation):
             pass
 
         else:
             # Send GET request to take the HTML DOC from the URI
             root_URI = requests.get(URI)
+            root_URI.encoding = 'utf-8'
 
             if root_URI.status_code == 200:
                 # Render HTML_DOC from string to beautifulsoup
                 # soup = bs(root_URI.text, 'html.parser')
 
                 resp = list()
-                resp.extend(re.findall(r'href=\"([^\"]*)\"', root_URI.text))
+                regexpattern = r'href=\"([^\"]*)\"'
+                resp.extend(regex.findall(regexpattern, root_URI.text))
                 # resp.extend(re.findall(r'url=\"([^\"]*)\"', root_URI.text))
                 # resp.extend(re.findall(r'src=\"([^\"]*)\"', root_URI.text))
                 relative_to_absolute(URI, resp)
 
                 for link in resp:
                     dictionary[link] = dict()
-                    # print(dictionary)
+                    print(dictionary)
                     if depth > 1:
                         adding(link, dictionary[link], depth-1)
 
